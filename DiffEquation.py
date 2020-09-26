a4c863bb377d8a8eede67793746974712d1a4a5b
Rahul Ambati <67880365+RahulAmbatiGahan@users.noreply.github.com>
2020-09-26 15:27:18 -0700

    Update DiffEquation.py

---
+++ DiffEquation.py
@@ -0,0 +1,7 @@
+"""
+Author: Rahul Ambati
+Description: Given the differential equation f'(x) = x^x,
+write a function that uses Euler's method to approximate the
+value of f(x1) given an initial condition (x0, f(x0))
+and the value of x1.
+"""
